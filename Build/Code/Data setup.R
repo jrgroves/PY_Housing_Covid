91d2b720ef6bae83211c236ee83ae373335d307c
Jeremy R. Groves <jgroves@niu.edu>
2023-03-24 16:50:15 -0500

    Census and Map updates

--- Build/Code/Data setup.R
+++ Build/Code/Data setup.R
@@ -1,4 +1,4 @@
-#Import Housing Data, Geocode for lat and long, and pull census info
+#Import Housing Data,  and pull census info
 #Jeremy R. Groves
 #Feb 27, 2023
 
@@ -6,40 +6,78 @@ rm(list=ls())
 
 library(tidyverse)
 library(readxl)
-library(tidygeocoder)
+library(sf)
 library(tidycensus)
 
-raw.data<-read_excel("./Build/Input/Housing sales data.xlsx")
+#Read in Raw Sales Data from PY and Others
+    raw.data<-read_excel("./Build/Input/Housing sales data.xlsx")
+    
+    data <- raw.data %>%
+      mutate(TMK = substr(gsub("-","",ParcelNumb),1,9),
+             TMK.condo = substr(gsub("-","",ParcelNumb),10,13))
+    
+    map.1<-st_read(dsn="./Build/Input/Maps/oahtmk.shp")
+    
+    map <- map.1 %>%
+      select(TMK9TXT, TAXPIN, REC_AREA_S, TYPE, STREET_PAR, GISAcres) %>%
+      rename("TMK" = "TMK9TXT")
 
+#Download ACS data and process maps
+    census <- get_acs(geography = "block group",
+                      variables = c(population = "B01001_001",
+                                    white = "B02001_002",
+                                    black = "B02001_003",
+                                    asian = "B02001_005",
+                                    hawaian = "B02001_006",
+                                    households = "B25002_001",
+                                    occupied = "B25002_002",
+                                    vacant = "B25002_003",
+                                    owner = "B25003_002",
+                                    renter = "B25003_003"),
+                      year = 2021,
+                      state = 15,
+                      geometry = TRUE)
+    
+    
+    cen.map <- census %>%
+      select(GEOID, geometry) %>%
+      distinct() %>%
+      st_transform(., crs=st_crs(map.sale))
 
-address <- raw.data %>%
-  select(ParcelNumb, Address) %>%
-  mutate(Address = str_replace(Address, "(\\d) (\\d)","\\1-\\2")) %>%
-  distinct(Address, .keep_all = TRUE) %>%
-  slice(1:10000) %>%
-  geocode(Address, method = 'census')
+#Create maps for visualizations and to obtain Census link
+    map.data <- data %>%
+      mutate(Sale = 1) %>%
+      select(TMK, Sale) %>%
+      distinct() 
+    
+    map.sale <- map %>%
+      full_join(., map.data, by="TMK") %>%
+      mutate(Sale = case_when(Sale == 1 ~ 1,
+                              TRUE ~ 0)) %>%
+      filter(!is.na(TAXPIN))
+    
+    sale.geo <- map.sale %>%
+      filter(Sale == 1) %>%
+      st_intersection(., cen.map) %>%
+      select(TMK, GEOID)
 
-address2 <- raw.data %>%
-  select(ParcelNumb, Address) %>%
-  mutate(Address = str_replace(Address, "(\\d) (\\d)","\\1-\\2")) %>%
-  distinct(Address, .keep_all = TRUE) %>%
-  slice(10001:20000) %>%
-  geocode(Address, method = 'census')
-
-address3 <- raw.data %>%
-  select(ParcelNumb, Address) %>%
-  mutate(Address = str_replace(Address, "(\\d) (\\d)","\\1-\\2")) %>%
-  distinct(Address, .keep_all = TRUE) %>%
-  slice(20001:24104) %>%
-  geocode(Address, method = 'census')
-
-address <- address %>%
-  bind_rows(address2) %>%
-  bind_rows(address3)
-
-b1<-address %>%
-  filter(is.na(lat))
-
-
-
+#Create core data set
+    cen.data <- census %>%
+      select(GEOID, variable, estimate) %>%
+      pivot_wider(names_from = variable, values_from = estimate, id_cols = GEOID) %>%
+      filter(population != 0,
+             households != 0) %>%
+      mutate(per_white = white/population,
+             per_black = black/population,
+             per_asian = asian/population,
+             per_hawaian = hawaian/population,
+             per_occupied = occupied/households,
+             per_vacant = vacant/households,
+             per_owner = owner/occupied,
+             per_renter = owner/occupied)
+    
+    core <- data %>%
+      left_join(., sale.geo, by = "TMK") %>%
+      left_join(., cen.data, by = "GEOID") %>%
+      filter(!is.na(population))
 
